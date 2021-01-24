585d0ed41dd9725450246d9b1b578e3181f1a967
parkerbenoit <72704200+parkerbenoit@users.noreply.github.com>
2021-01-24 13:47:34 -0800

    have light and dark mode buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
-    $("#walrus-showing").slideToggle();
-    $("#walrus-hidden").slideToggle();
+//  $(".clickable").click(function() {
+ //   $("#walrus-showing").slideToggle();
+ //   $("#walrus-hidden").slideToggle();
+ // });
+
+  $("button#dark-mode").click(function() {
+    $("body").removeClass("light");
+    $("body").addClass("dark");
   });
+  $("button#light-mode").click(function() {
+    $("body").removeClass("dark");
+    $("body").addClass("light");
+  });
+
 });
+
+
